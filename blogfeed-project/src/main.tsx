645cb4a5655c4fe7d9fdb93c8345f341394ef7bc
Moses Ekpenisi <moses.ekpenisi@edu.fh-kaernten.ac.at>
2024-01-31 18:01:06 +0100

    Added the complete blogfeed project code

--- blogfeed-project/src/main.tsx
+++ blogfeed-project/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App";
+import BlogFullPage from "./pages/BlogFullPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/blog/:id",
+    element: <BlogFullPage/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <RouterProvider router={router} />
+);
